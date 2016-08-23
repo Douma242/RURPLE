0fbd74722cc53adbb0c74c24da1d688e09f0e819
Douma242 <danieldouma@hotmail.es>
2016-08-23 09:32:03 -0600

    :_V

---
+++ fisha.py
@@ -0,0 +1,13 @@
+class Fisha():
+	def __init__(self, forma, posicion):
+		self.posicion= posicion
+		self.forma= forma
+		self.agarrada= True
+
+	def agarrar(self):
+			if self.agarrada:
+				self.agarrada=False
+
+
+		
+		
