0f36ffcb999bbcad0f23a37ba48be2bf98d9f600
Douma242 <danieldouma@hotmail.es>
2016-08-24 09:41:12 -0600

    D:

--- claseMapa.py
+++ claseMapa.py
@@ -1,4 +1,8 @@
 class Mapa():
 	def __init__(self, posicionR, posicionF):
 		
+		self.posicionR=[0[0]]
+
+		self.posicionF=
+		
 		
--- mapa.py
+++ mapa.py
@@ -1,9 +1,15 @@
+from claseMapa import Mapa
+class Mapita(Mapa):
+	def __init__(self, posicionR, posicionF):
+		super(). __init__(posicionR, posicionF)
 
-def mapitas(mapa):
-	mapitas=[]
-	f= open(mapa, "r")
-	for linea in f:
-		l=list(linea.strip())
-		mapitas.append(l)
-	return mapitas
-	f.close()
+	def 
+
+	def mapitas(mapa):
+		mapitas=[]
+		f= open(mapa, "r")
+		for linea in f:
+			l=list(linea.strip())
+			mapitas.append(l)
+		return mapitas
+		f.close()
